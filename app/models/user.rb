e4fe82a4e6ac1a3c3f41b859ff477d914e56d9fe
HIRATA RIKAKO <hiratarikako@HIRATAnoMacBook-Air.local>
2022-08-04 19:56:44 +0900

    パスワードのバリデーション　修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+                
   validates :nickname,        presence: true
-  validates :first_name,      presence: true, format: { with:/\A[ぁ-んァ-ン一-龥]+\z/}
+  validates :first_name,      presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
   validates :last_name,       presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
   validates :first_name_kana, presence: true, format: { with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/}
   validates :last_name_kana,  presence: true, format: { with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/}
   validates :birthday,        presence: true
+
+  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
 end
