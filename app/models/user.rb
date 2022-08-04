de9484f1d0f9557856e8f52b750ce32d9aad87e1
HIRATA RIKAKO <hiratarikako@HIRATAnoMacBook-Air.local>
2022-08-04 19:41:04 +0900

    名前のバリデーション　修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname,        presence: true
-  validates :first_name,      presence: true
-  validates :last_name,       presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana,  presence: true
+  validates :first_name,      presence: true, format: { with:/\A[ぁ-んァ-ン一-龥]+\z/}
+  validates :last_name,       presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
+  validates :first_name_kana, presence: true, format: { with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/}
+  validates :last_name_kana,  presence: true, format: { with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/}
   validates :birthday,        presence: true
 end
