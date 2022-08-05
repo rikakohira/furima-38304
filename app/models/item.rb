f346c0ea6e31dc990223b9b9482f7c0bcfeb85d9
HIRATA RIKAKO <hiratarikako@HIRATAnoMacBook-Air.local>
2022-08-05 21:28:44 +0900

    テストコード　実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   validates :item_name, presence: true
   validates :content, presence: true
-  validates :price, presence: true, numericality: { only_integer: true, in: 300..9999999 }
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
   validates :image, presence: true
   validates :category_id, numericality: { other_than: 1, message: "can't be blank"} 
   validates :condition_id, numericality: { other_than: 1, message: "can't be blank"} 
