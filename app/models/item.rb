662e05b544c3c2c6894077321cda65fe9b35fdee
HIRATA RIKAKO <hiratarikako@HIRATAnoMacBook-Air.local>
2022-08-05 14:32:20 +0900

    ActiveHash　実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,19 @@ class Item < ApplicationRecord
   validates :content, presence: true
   validates :price, presence: true, numericality: { only_integer: true, in: 300..9999999 }
   validates :image, presence: true
-  
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank"} 
+  validates :condition_id, numericality: { other_than: 1, message: "can't be blank"} 
+  validates :shippingfee_id, numericality: { other_than: 1, message: "can't be blank"} 
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank"} 
+  validates :delivery_day_id, numericality: { other_than: 1, message: "can't be blank"} 
+
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shippingfee
+  belongs_to :prefecture
+  belongs_to :delivery_day
 end
