cb28e6d54b43c7a058839adb36193e2406bd2366
HIRATA RIKAKO <hiratarikako@HIRATAnoMacBook-Air.local>
2022-08-05 13:45:45 +0900

    商品投稿ページ　一部実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,8 @@ class Item < ApplicationRecord
   validates :item_name, presence: true
   validates :content, presence: true
   validates :price, presence: true, numericality: { only_integer: true, in: 300..9999999 }
- 
+  validates :image, presence: true
+  
   belongs_to :user
+  has_one_attached :image
 end
