2f3f0c85241024dbd8a90ca6324981a1db684778
HIRATA RIKAKO <hiratarikako@HIRATAnoMacBook-Air.local>
2022-08-08 14:41:49 +0900

    商品削除機能　実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
   before_action :set_item, only: [:edit, :show]
 
   def index
@@ -32,6 +32,13 @@ class ItemsController < ApplicationController
       redirect_to item_path
     else
       render :edit
+    end
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    if item.destroy
+	    redirect_to root_path
     end
   end
 
