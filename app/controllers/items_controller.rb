1a58469fddb755c6ddb8d5e85928c5dad08c0633
HIRATA RIKAKO <hiratarikako@HIRATAnoMacBook-Air.local>
2022-08-08 12:35:22 +0900

    Rubocop　実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,12 +23,12 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.find(params[:id])   
+    @item = Item.find(params[:id])
     redirect_to root_path unless current_user.id == @item.user_id
   end
 
   def update
-    @item = Item.find(params[:id]) 
+    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
