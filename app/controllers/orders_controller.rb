94b5312139a7170b1a1af70777697c76e79b393b
HIRATA RIKAKO <hiratarikako@HIRATAnoMacBook-Air.local>
2022-08-09 17:32:40 +0900

    Formオブジェクト　実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,32 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item
 
   def index
-    
+    if current_user.id == @item.user_id
+      redirect_to root_path
+    else
+      @order_delivery = OrderDelivery.new 
+    end
   end
 
   def create
-    #Comment.create(comment_params)
-		#redirect_to "/tweets/#{comment.tweet.id}"
+    @order_delivery = OrderDelivery.new(order_params)
+    if @order_delivery.valid?
+       @order_delivery.save
+       redirect_to root_path
+    else
+      render :index
+		end
   end
 
-  
+  private
+
+  def order_params
+    params.require(:order_delivery).permit(:post_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end  
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
 end
