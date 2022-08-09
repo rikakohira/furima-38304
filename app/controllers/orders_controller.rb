0a51781ff94277e26e616af494919a5f0e1b714f
HIRATA RIKAKO <hiratarikako@HIRATAnoMacBook-Air.local>
2022-08-09 14:25:32 +0900

    購入ページへ遷移　実装

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,13 @@
+class OrdersController < ApplicationController
+
+  def index
+    
+  end
+
+  def create
+    #Comment.create(comment_params)
+		#redirect_to "/tweets/#{comment.tweet.id}"
+  end
+
+  
+end
